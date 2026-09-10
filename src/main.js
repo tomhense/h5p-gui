d6f54b9b0954542997fdca4de09f8ebf50890f4c
Tom <example.com>
2026-09-10 14:20:05 +0200

    Clear loading status after player startup

--- src/main.js
+++ src/main.js
@@ -58,6 +58,7 @@ async function openPath(path) {
   const root = await invoke('serve_h5p', { path });
   container.replaceChildren();
   await new H5P(container, { h5pJsonPath: root, contentJsonPath: `${root}/content`, librariesPath: root, embedType: 'div', frame: true, fullScreen: true, frameJs: frameJsAsset, frameCss: frameCssAsset });
+  status('');
 }
 
 async function openFile(file) {
